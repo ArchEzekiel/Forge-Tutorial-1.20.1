628ecd4f14298a0eca12f069b0cc3c0fef4ded29
ArchEzekiel <66336623+ArchEzekiel@users.noreply.github.com>
2025-05-29 19:54:55 -0500

    updates to #5, all working (renamed the end_stone_sapphire_ore.png to end_sapphire_ore.png

--- src/main/java/net/arch/tutorialmod/TutorialMod.java
+++ src/main/java/net/arch/tutorialmod/TutorialMod.java
@@ -1,6 +1,10 @@
 package net.arch.tutorialmod;
 
 import com.mojang.logging.LogUtils;
+import net.arch.tutorialmod.block.ModBlocks;
+import net.arch.tutorialmod.item.ModCreativeModeTabs;
+import net.arch.tutorialmod.item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -27,6 +31,10 @@ public class TutorialMod
     {
         IEventBus modEventBus = context.getModEventBus();
 
+        ModCreativeModeTabs.register(modEventBus);
+
+        ModItems.register(modEventBus);
+        ModBlocks.register(modEventBus);
         // Register the commonSetup method for modloading
         modEventBus.addListener(this::commonSetup);
 
@@ -48,7 +56,11 @@ public class TutorialMod
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event)
     {
-
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS)
+        {
+            event.accept(ModItems.SAPPHIRE);
+            event.accept(ModItems.RAW_SAPPHIRE);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
